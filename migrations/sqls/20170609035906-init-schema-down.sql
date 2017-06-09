6d2e519197e1dceae02df7936dc21d8356cb3705
Arturo Volpe <arturovolpe@gmail.com>
2017-06-09 00:09:38 -0400

    Add first database migration

---
+++ migrations/sqls/20170609035906-init-schema-down.sql
@@ -0,0 +1,5 @@
+/* Replace with your SQL commands */
+drop table if exists link;
+drop table if exists user_project;
+drop table if exists project;
+drop table if exists "user";
---
+++ migrations/sqls/20170609035906-init-schema-up.sql
@@ -0,0 +1,46 @@
+/* Replace with your SQL commands */
+
+CREATE SEQUENCE user_id_sequence INCREMENT 1 START 1;
+CREATE TABLE "user"
+(
+  id bigint NOT NULL DEFAULT nextval('user_id_sequence'),
+  email text NOT NULL,
+  name text NOT NULL,
+  password text NOT NULL,
+  salt text NOT NULL,
+  CONSTRAINT user_pkey PRIMARY KEY (id)
+);
+
+CREATE SEQUENCE project_id_sequence INCREMENT 1 START 1;
+CREATE TABLE "project"
+(
+  id bigint NOT NULL DEFAULT nextval('project_id_sequence'),
+  name text NOT NULL,
+  description text,
+  CONSTRAINT project_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE "user_project"
+(
+  user_id bigint NOT NULL,
+  project_id bigint NOT NULL,
+  role text,
+  CONSTRAINT "user_project-pkey" PRIMARY KEY (user_id, project_id),
+  CONSTRAINT "user_project-user"    FOREIGN KEY (user_id)    REFERENCES "user"  (id) MATCH SIMPLE,
+  CONSTRAINT "user_project-project" FOREIGN KEY (project_id) REFERENCES project (id) MATCH SIMPLE
+);
+
+CREATE SEQUENCE link_id_sequence INCREMENT 1 START 1;
+CREATE TABLE "link"
+(
+  id bigint NOT NULL DEFAULT nextval('link_id_sequence'),
+  href text NOT NULL,
+  description text,
+
+  project_id bigint,
+  image text,
+
+  role text,
+  CONSTRAINT link_pkey PRIMARY KEY (id),
+  CONSTRAINT "link-project" FOREIGN KEY (project_id) REFERENCES project (id) MATCH SIMPLE
+);
